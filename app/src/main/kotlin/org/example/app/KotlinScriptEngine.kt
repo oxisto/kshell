007ee7685bd39ff56b5389a9387e3f6b805a1324
Christian Banse <oxisto@aybaze.com>
2025-02-08 23:24:53 +0100

    Very crude way to feed variables back to script
    Not sure if this really a good way, but it sort of works for now.

--- app/src/main/kotlin/org/example/app/KotlinScriptEngine.kt
+++ app/src/main/kotlin/org/example/app/KotlinScriptEngine.kt
@@ -5,10 +5,19 @@ import org.jline.console.ScriptEngine
 import org.jline.reader.Completer
 import java.io.File
 import java.nio.file.Path
+import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredMembers
+import kotlin.script.experimental.api.*
 import kotlin.script.experimental.host.StringScriptSource
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 
 class KotlinScriptEngine : ScriptEngine {
+
+    val variables = mutableMapOf<String, Any?>()
+
+    val evaluatedSnippets = mutableListOf<Any>()
 
     val host: BasicJvmScriptingHost
 
@@ -36,19 +45,21 @@ class KotlinScriptEngine : ScriptEngine {
         return false
     }
 
-    override fun put(name: String?, value: Any?) {
+    override fun put(name: String, value: Any?) {
         println("Putting $name = $value")
+        variables[name] = value
     }
 
-    override fun get(p0: String?): Any? {
-        TODO("Not yet implemented")
+    override fun get(name: String): Any? {
+        println("Getting $name")
+        return variables[name]
     }
 
     override fun find(p0: String?): Map<String?, Any?>? {
         TODO("Not yet implemented")
     }
 
-    override fun del(vararg p0: String?) {
+    override fun del(vararg name: String?) {
         // TODO
     }
 
@@ -91,10 +102,75 @@ class KotlinScriptEngine : ScriptEngine {
     override fun execute(stmt: String): Any? {
         println("Executing: $stmt")
 
+        // Not sure why jline does this
+        if(stmt == "_executionResult ? 0 : 1") {
+            val result = get("_executionResult")
+            return if(result != null) {
+                0
+            } else {
+                1
+            }
+        }
+
+
         val source = StringScriptSource(stmt)
 
-        val resultWith = host.eval(source, DefaultScriptConfiguration, null)
+        val scriptConfig = ScriptCompilationConfiguration({
+            defaultImports(
+                "org.example.app.*"
+            )
+            providedProperties(*variables.filter { it.value != null }.map { it.key to KotlinType(it.value!!::class) }.toTypedArray())
+            jvm {
+                dependenciesFromCurrentContext(
+                    "kotlin-scripting-jvm",
+                    "app"
+                )
+            }
+        })
+
+        val evalConfig = if (evaluatedSnippets.isEmpty()) {
+            ScriptEvaluationConfiguration {
+                /*jvm {
+                    scriptsInstancesSharing(true)
+                }*/
+            }
+        } else {
+            ScriptEvaluationConfiguration {
+                /*jvm {
+                    scriptsInstancesSharing(true)
+                }*/
+                //previousSnippets(*evaluatedSnippets.toTypedArray())
+                providedProperties(*variables.filter { it.value != null }.map { it.key to it.value }.toTypedArray())
+            }
+        }
+
+        val resultWith = host.eval(source, scriptConfig, evalConfig)
         println(resultWith)
+
+        if(resultWith is ResultWithDiagnostics.Success) {
+            val resultValue = resultWith.value.returnValue
+            val scriptInstance = resultValue.scriptInstance
+
+            if( scriptInstance != null) {
+                evaluatedSnippets += scriptInstance
+
+                scriptInstance::class.declaredMembers.forEach { callable ->
+                    variables[callable.name] = callable.call(scriptInstance)
+                }
+            }
+
+            return when(resultValue) {
+                is ResultValue.Unit -> {
+                    null
+                }
+
+                is ResultValue.Value -> {
+                    resultValue.value
+                }
+                is ResultValue.Error -> TODO()
+                ResultValue.NotEvaluated -> TODO()
+            }
+        }
 
         return null
     }
